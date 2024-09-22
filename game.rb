3a3f45b756d8050c8131a145bd1cabd5b1a5881c
InAweofMyTism <inaweofmytism@gmail.com>
2024-09-22 19:38:27 -0400

    initial commit
    add base color choices to game class and game
    instructions in main

---
+++ game.rb
@@ -0,0 +1,13 @@
+class Game
+  @@color_options = ["red", "blue", "yellow", "purple", "green", "orange", "brown", "white", "black"]
+
+  def initialize()
+    #do some code
+  end
+
+  def create_code()
+  end
+
+  def check_answers()
+  end
+end
